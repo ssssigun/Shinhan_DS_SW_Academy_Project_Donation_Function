092292b90dc75360046cdd26bc57bfae7f89db34
seo <95337086+Yunseoyoung@users.noreply.github.com>
2023-08-09 16:58:09 +0900

    fix: copy 지움

--- delivery_donation_service_backend/src/main/java/kr/co/main/selectList/MenuRepository.java
+++ delivery_donation_service_backend/src/main/java/kr/co/main/selectList/MenuRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MenuRepository extends JpaRepository<Menu,Integer>{
 	List<Menu> findByStorePk(int store_pk);
 }
