057403d1bc6acfcf138650abe0bdbb703d2d438b
ssssigun <109648080+ssssigun@users.noreply.github.com>
2023-08-09 20:51:26 +0900

    fix : cart entity 필드 private 변경

--- delivery_donation_service_backend/src/main/java/kr/co/main/cart/Cart.java
+++ delivery_donation_service_backend/src/main/java/kr/co/main/cart/Cart.java
@@ -5,15 +5,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import kr.co.main.selectList.Menu;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
+@ToString(exclude="menu")
 @Entity
 @Table(name = "Cart")
 public class Cart {
@@ -21,17 +24,21 @@ public class Cart {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "cart_pk")
-	public int cartPk;
+	private int cartPk;
 	
 	@Column(name = "flag")
-	public int flag;
+	private int flag;
 	
 	@Column(name = "user_pk")
-	public int userPk;
+	private int userPk;
 	
 	@Column(name = "menu_pk")
-	public int menuPk;
+	private int menuPk;
 	
 	@Column(name="amount")
-	public int amount;
+	private int amount;
+	
+	@ManyToOne
+	@JoinColumn(name="menu_pk")
+	private Menu menu;
 }
