97990ebdd85e354ed36bba1e7578a06e3790e4f4
doobab <114578342+doobab@users.noreply.github.com>
2023-08-13 21:46:18 +0900

    fix: 각종 오류 해결

--- delivery_donation_service_frontend/src/storeList/component/StoreListHeader.js
+++ delivery_donation_service_frontend/src/storeList/component/StoreListHeader.js
@@ -3,14 +3,14 @@ import BackIcon from '../../common/component/Icons/BackIcon';
 import HomeIcon from '../../common/component/Icons/HomeIcon';
 import CartIcon from '../../common/component/Icons/CartIcon';
 
-const StoreListHeader = ({ children }) => {
+const StoreListHeader = ({ children, backUrl }) => {
   return (
     <HeaderWrapper>
-      <div class="headerLeftSide">
-        <BackIcon size="30px" color="#000" />
+      <div className="headerLeftSide">
+        <BackIcon size="30px" color="#000" url={backUrl} />
       </div>
       {children}
-      <div class="headerRightSide">
+      <div className="headerRightSide">
         <HomeIcon size="20px" color="#000" />
         <CartIcon size="24px" color="#000" />
       </div>
--- delivery_donation_service_frontend/src/storeList/page/storeListFree.js
+++ delivery_donation_service_frontend/src/storeList/page/storeListFree.js
@@ -1,95 +1,106 @@
 //가게 목록 페이지
 
 //컴포넌트 불러오기
-import SortOptionBar from "../component/sortOptionBar";
-import Store from "../component/store";
-import StoreListHeader from"../component/StoreListHeader";
+import SortOptionBar from '../component/sortOptionBar';
+import Store from '../component/store';
+import StoreListHeader from '../component/StoreListHeader';
 
 //json 불러오기
-import categoryData from '../../common/json/category.json'
-import StoreData from '../../common/json/storeTest.json'
+import categoryData from '../../common/json/category.json';
+import StoreData from '../../common/json/storeTest.json';
 
 // 모듈 불러오기
-import { useNavigate,useLocation } from "react-router-dom";
-import React, { useEffect, useState } from 'react';
-import axios from'axios';
+import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 // scss 불러오기
-import "../../common/style/reset.scss"
-import "../../common/style/common.scss"
-import "../style/storeList.scss"
+import '../../common/style/reset.scss';
+import '../../common/style/common.scss';
+import '../style/storeList.scss';
 
 const StoreList = () => {
   const navigate = useNavigate();
-    function move(e){
-      navigate("/storeMainFree", {
-        state:{
-          storePk : e.storePk
-        }
-      });
-    }
-    // 카테고리 이름 가져오기 위해서 location 사용
-    let location = useLocation();
+  function move(e) {
+    navigate('/storeMainFree', {
+      state: {
+        storePk: e.storePk,
+        category: checkedMenuBar,
+      },
+    });
+  }
+  // 카테고리 이름 가져오기 위해서 location 사용
+  let location = useLocation();
+  const cate = location.state.state ? location.state.state.name : location.state.name;
 
-    
-    const [checkedMenuBar, setCheckedMenuBar] = useState(location.state.name);
-    const [storeData, setStoreData] = useState([]);
+  const [checkedMenuBar, setCheckedMenuBar] = useState(cate);
+  const [storeData, setStoreData] = useState([]);
 
-    // GET 요청
-    function selectList(cate){
-      axios.get(`/db/selectStore?category=${cate}`)
-      .then(response => {
+  // GET 요청
+  function selectList(cate) {
+    axios
+      .get(`/db/selectStore?category=${cate}`)
+      .then((response) => {
         // 성공 처리
         setStoreData(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         // 에러 처리
         console.error(error);
       });
-    }
-    
-    //처음 렌더링 시 실행
-    useEffect(() =>{
-      selectList(location.state.name);
-    },[])
-    return (
-      
-      <div id="storeListWrapper">
-        <div className="storeListTopArea">
-            {/* 헤더 */}
-            <StoreListHeader>
-              {checkedMenuBar}
-            </StoreListHeader>
-          {/* 메뉴 카테고리 */}
-          <div id="categoryMenuBar">
-            <ul id="menu">
-              {
-                categoryData.category.map(category => (
-                  checkedMenuBar === category.name ?
-                  <li className="checked"><p>{category.name}</p></li>
-                  :
-                  <li onClick={() => { setCheckedMenuBar(category.name); selectList(category.name)}}><p>{category.name}</p></li>
-                ))
-              }
-            </ul>
-          </div>
-          {/* 가게 정렬 옵션*/}
-          <SortOptionBar/>
-        </div>
-        {/* 가게 목록들 */}
-        <div className="storeListBottomArea">
-          <ul className="storeListWrap">
-            {
-              StoreData.map(store => (
-                  <li className="store" key={store.storePk} onClick={()=>move(store)}>
-                    <Store st={store}/>
-                  </li>
-                )
-              )
-            }
+  }
+
+  //처음 렌더링 시 실행
+  useEffect(() => {
+    selectList(cate);
+  }, []);
+
+  // 카테고리 포커스
+  const ref = useRef({});
+  useEffect(() => {
+    ref.current[checkedMenuBar].scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'end' });
+  }, [checkedMenuBar]);
+
+  return (
+    <div id="storeListWrapper">
+      <div className="storeListTopArea">
+        {/* 헤더 */}
+        <StoreListHeader backUrl="/">{checkedMenuBar}</StoreListHeader>
+        {/* 메뉴 카테고리 */}
+        <div id="categoryMenuBar">
+          <ul id="menu">
+            {categoryData.category.map((category) =>
+              checkedMenuBar === category.name ? (
+                <li className="checked" ref={(el) => (ref.current[category.name] = el)}>
+                  <p>{category.name}</p>
+                </li>
+              ) : (
+                <li
+                  onClick={() => {
+                    setCheckedMenuBar(category.name);
+                    selectList(category.name);
+                  }}
+                  ref={(el) => (ref.current[category.name] = el)}
+                >
+                  <p>{category.name}</p>
+                </li>
+              ),
+            )}
           </ul>
         </div>
+        {/* 가게 정렬 옵션*/}
+        <SortOptionBar />
       </div>
-    );
-  };
-  export default StoreList;
-   
+      {/* 가게 목록들 */}
+      <div className="storeListBottomArea">
+        <ul className="storeListWrap">
+          {StoreData.map((store) => (
+            <li className="store" key={store.storePk} onClick={() => move(store)}>
+              <Store st={store} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default StoreList;
