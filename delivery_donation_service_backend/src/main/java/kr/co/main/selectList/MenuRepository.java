445a7f2d7e2b15fe26c8a42b9fd072e68a0eb52d
doobab <doobab99@gmail.com>
2023-08-16 17:30:52 +0900

    fix: 수량 오류 해결

--- delivery_donation_service_backend/src/main/java/kr/co/main/selectList/MenuRepository.java
+++ delivery_donation_service_backend/src/main/java/kr/co/main/selectList/MenuRepository.java
@@ -10,6 +10,6 @@ import org.springframework.data.repository.query.Param;
 public interface MenuRepository extends JpaRepository<Menu,Integer>{
 	List<Menu> findByStorePk(int store_pk);
 	
-	@Query(value="SELECT donated_amount FROM Menu m JOIN Order_detail o ON m.menu_pk = o.menu_pk WHERE o.menu_pk= :menuPk AND can_donate = 1", nativeQuery=true)
+	@Query(value="SELECT donated_amount FROM Menu m JOIN Order_detail o ON m.menu_pk = o.menu_pk WHERE o.menu_pk= :menuPk AND can_donate = 1 GROUP BY m.menu_pk", nativeQuery=true)
 	Object countAmount(@Param("menuPk") int menuPk);
 }
